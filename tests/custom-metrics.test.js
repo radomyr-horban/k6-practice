74b7ec07b0401d7e2cbdb197587988d6b9bad1c0
Radomyr Horban <horban.radomyr@gmail.com>
2025-06-14 07:15:43 +0300

    Custom metrics

---
+++ tests/custom-metrics.test.js
@@ -0,0 +1,26 @@
+import http from "k6/http";
+import { sleep } from "k6";
+import { Counter, Trend } from "k6/metrics";
+
+export const options = {
+  vus: 5,
+  duration: "5s",
+  thresholds: {
+    http_req_duration: ["p(95)<200"],
+    my_counter: ["count>10 "],
+    response_time_news_page: ["p(95)<150", "p(99)<200"] // p(99)<200 threshold is not displayed in the report, but we can still see if it pasees
+  }
+};
+
+let myCounter = new Counter("my_counter");
+let newsPageResponseTrend = new Trend("response_time_news_page");
+
+export default function () {
+  let res = http.get("https://quickpizza.grafana.com/test.k6.io/");
+  myCounter.add(1);
+  sleep(1);
+
+  res = http.get("https://quickpizza.grafana.com/news.php");
+  newsPageResponseTrend.add(res.timings.duration);
+  sleep(1);
+}
